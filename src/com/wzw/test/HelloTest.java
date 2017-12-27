94c72d0734e2121706c2c7d738ca2b697df964a0
wzw <946906782@qq.com>
2017-12-27 17:08:21 +0800

    解决冲突

--- src/com/wzw/test/HelloTest.java
+++ src/com/wzw/test/HelloTest.java
@@ -4,10 +4,6 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("hello......");
 		System.out.println("world......");
-<<<<<<< HEAD
-		System.out.println("......");
-=======
 		System.out.println("has......");
->>>>>>> branch 'master' of ssh://git@github.com/wangzongwen/day01.git
 	}
 }
