ed139e327c6c9a2e5861ed8d3a0dd2263bf3ae45
wzw <946906782@qq.com>
2017-12-27 15:17:43 +0800

    提交到本地仓库

--- src/com/wzw/test/HelloTest.java
+++ src/com/wzw/test/HelloTest.java
@@ -3,6 +3,6 @@ package com.wzw.test;
 public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("hello......");
-		System.out.println("world......");
+		System.out.println("hello......");
 	}
 }
