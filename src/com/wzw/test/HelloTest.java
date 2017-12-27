67b9814ad56333172c924f58343af3879e86292e
wzw <946906782@qq.com>
2017-12-27 17:01:23 +0800

    冲突测试

--- src/com/wzw/test/HelloTest.java
+++ src/com/wzw/test/HelloTest.java
@@ -4,8 +4,5 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("hello......");
 		System.out.println("world......");
-		System.out.println("......");
-		System.out.println("will......");
-		System.out.println("has......");
 	}
 }
